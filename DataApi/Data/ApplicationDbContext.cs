b488f2867a74fd78088025eb5fd4600247df7ad8
Muatasim-Aswad <malasoad2021@gmail.com>
2025-08-20 23:05:58 +0200

    eg 2008

---
+++ DataApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+using DataApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataApi.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+{
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Configure relationships, constraints, etc.
+        modelBuilder.Entity<Product>()
+            .HasOne(p => p.Category)
+            .WithMany(c => c.Products)
+            .HasForeignKey(p => p.CategoryId);
+    }
+}
